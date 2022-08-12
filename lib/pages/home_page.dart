42b4543335eba84357baf828d112145fa16184b8
Deepanshu Tomar <Deepanshu@gmail.com>
2022-08-13 01:44:17 +0530

    Day 14 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -53,8 +53,45 @@ class _HomepageState extends State<Homepage> {
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
             ? GridView.builder(
-                gridDelegate: GridDelegate,
-                itemBuilder: itemBuilder,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
+                ),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: GridTile(
+                        header: Container(
+                          child: Text(
+                            item.name,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                          ),
+                        ),
+                        child: Image.network(
+                          item.image,
+                        ),
+                        footer: Container(
+                          child: Text(
+                            // item.name,
+                            item.price.toString(),
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                          ),
+                        ),
+          
+                      ));
+                },
                 itemCount: CatalogModel.items.length,
               )
             // ListView.builder(
